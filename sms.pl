658596c6a93ea449c1cd7089acc1ac130f460e53
diff3 <magnus@entropy.nu>
2024-04-13 12:28:24 +0200

    updated sms.pl

--- sms.pl
+++ sms.pl
@@ -7,8 +7,13 @@ use utf8;
 
 my $dt = DateTime->now;
 
+if (scalar(@ARGV) != 1) {
+    die "Usage: $0 <phonenumer with landcode>\n";
+}
+
+my $arg = $ARGV[0];
+
 say C4::SMS->send_sms( {
-    destination => "+46...",
+    destination => "$arg",
     message     => "test æøå ÆØÅ åäö ÅÄÖ $dt",
 } );
-
